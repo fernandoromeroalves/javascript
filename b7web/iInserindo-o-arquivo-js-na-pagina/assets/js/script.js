9b95e470f86afdfc18edac85f0fe52b74f0aa214
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-07-24 20:14:31 -0300

    .

---
+++ b7web/iInserindo-o-arquivo-js-na-pagina/assets/js/script.js
@@ -0,0 +1,9 @@
+let nome = "Romero"
+let idade = 90
+let estado = "Rio Grande do sul"
+let cidade = "Eldorado do Sul"
+
+console.log(nome)
+console.log(idade)
+console.log(estado)
+console.log(cidade)
