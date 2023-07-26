aa0efd39759498d65973d07269475966f4fc6912
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-07-26 04:38:15 -0300

    .

---
+++ desafios/d001/script.js
@@ -0,0 +1,9 @@
+function clicou() {
+    window.alert('você clicou no 1º botão')
+}
+function clicou2() {
+    window.alert('você clicou no 2º botão')
+}
+function clicou3() {
+    window.alert('você clicou no 3º botão')
+}
