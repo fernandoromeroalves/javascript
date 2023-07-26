0a262b999f642199df6ac9ba46794852d741873e
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-07-26 14:04:03 -0300

    .

---
+++ b7web/tipos-de-dados/assets/js/script.js
@@ -0,0 +1,18 @@
+let nome = "fernando" // string
+let idade = 90 // number
+let prof = "programador"
+let logado = true //boolean
+let deslogado = false //boolean
+let lista =["ovo, farinha, merengue"] //array
+let nomecompleto = { nome: "fernando", idade: 37}
+
+console.log(nome)
+console.log(typeof nome)
+console.log(idade)
+console.log(typeof idade)
+console.log(prof)
+console.log(logado)
+console.log(deslogado)  
+console.log(lista)
+console.log(nomecompleto)
+console.log(typeof nomecompleto) //typeof indica qual tipo de variavel
--- desafios/d002/script.js
+++ desafios/d002/script.js
@@ -1,6 +1,6 @@
 function inicio() {
-    let nome = window.prompt('qual seu nome?')
-    let idade = window.prompt('qual sua idade?')
-    let prof = window.prompt('qual sua profissão?')
-    window.alert("olá, ${nome}! de ${idade} e profissão ${prof} é um prazer te comhecer")
+    let nome = window.prompt('qual seu nome?');
+    let idade = window.prompt('qual sua idade?');
+    let prof = window.prompt('qual sua profissão?');
+    window.alert(`olá, ${nome}! de ${idade} anos e profissão ${prof} é um prazer te conhecer`)
 }
