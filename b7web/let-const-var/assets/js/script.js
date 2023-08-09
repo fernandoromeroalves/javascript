0dc8c1252ec9c94220a543abf7954c715f5ab02c
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-09 17:52:58 -0300

    .

--- b7web/let-const-var/assets/js/script.js
+++ b7web/let-const-var/assets/js/script.js
@@ -645,3 +645,9 @@ if(frutas.includes('uva')) {
 /* O que é DOM?  */
 
 
+function clicou() {
+    console.log("clicou no botão")
+}
+
+let botao = document.querySelector('.botao');
+botao.addEventListener("click",() => {clicou()} )
