44a61ec4711cef45636762b328901ab791f582a8
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-15 19:45:40 -0300

    requisições

---
+++ b7web/o-que-e-sincrono-e-assíncrono/assets/js/script.js
@@ -0,0 +1,18 @@
+/* 
+o que é sincrono e assincrono
+sncrono carrega em ordem crescente.
+assincrono não carrega em ordem 
+
+como a web funciona( requisição e respota)
+request = requisição
+response = resposta
+
+requisição, envio de dados em solicitação
+cabeçalhos / headers
+corpo / body geralmente vai vazio
+
+resposta 
+cabeçalhos - headers
+corpo - body retorna com dados
+*/
+
