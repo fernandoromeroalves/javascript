7316940d9e78c33c96641a63f788f5cc95c46980
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-16 13:51:35 -0300

    async e await

--- b7web/fazendo-uma-requisicao-na-pratica/assets/js/script.js
+++ b7web/fazendo-uma-requisicao-na-pratica/assets/js/script.js
@@ -2,24 +2,35 @@
 
 //entendo promise = promessa
 
-function clicou(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-        return response.json();
-    })
-    .then((json) => {
-        alert(`titulo do primeiro post ${json[0].title}`)
-    })
-    .catch(() => {
-        //catch executa se a requisição der erro
-        alert('deu problema na requisição')        
-    })
-    .finally(() => {
-        //executa por ultimo sempre
-        alert('opa acabou tudo')
-    })
+async function clicou(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+    alert(`titulo do primeiro post ${json[0].title}`)
+    
+    alert("clicou!!")
+}
+
+async function inserir(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts', 
+    {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'titulo de teste',
+            body: 'corpo de teste',
+            userId: 2
+        })
+    });
+    let json = await response.json();
+    console.log(json);
+
+
+   
 }
 
 
-document.querySelector('#botao').addEventListener('click', clicou)
+document.querySelector('#botao').addEventListener('click', clicou);
+document.querySelector('#inserir').addEventListener('click', inserir)
 
