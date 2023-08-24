3f4d2060c3ec8658e1a7d5dc5484cbb65cf6b0a4
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-24 00:10:57 -0300

    template strings

---
+++ b7web/template-strings/assets/js/script.js
@@ -0,0 +1,7 @@
+let nome = 'fernando';
+let idade = 37;
+
+//let frase = 'meu nome é '+nome+' e minha idade é '+ idade+' anos e ano que vem farei '+(idade+1)+' anos'
+let frase = `meu nome é ${nome} e minha idade é ${idade} anos e ano que vem farei ${idade+1} anos`
+
+console.log(frase);
