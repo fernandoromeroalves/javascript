fa09462446391bae090ceb4be7d35b93372f4f18
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-11 03:38:07 -0300

    .

--- bolsa-santandercoders/estruturas-de-repeticao-laços-condicionais.js
+++ bolsa-santandercoders/estruturas-de-repeticao-laços-condicionais.js
@@ -1,9 +1,14 @@
-// laços condicionais
+
 const input = require('readline-sync')
 
 const numeroSorteado = 5;
 
-const numero = input.question('qual numero você escolhe? ')
+let numero = Number(input.question('qual numero você escolhe? '))
 
 console.log(numero, typeof numero)
     
+if(numero ===numeroSorteado){
+    console.log('você acertou')
+} else{
+    console.log('você errou')
+}
