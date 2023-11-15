94c3400e3f94bc69d278b603f95cd97f75c73758
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-15 01:08:50 -0300

    Update funcoes.js

---
+++ bolsa-santandercoders/funcoes.js
@@ -0,0 +1,17 @@
+const arr1 = [30, 12, 45, 34, 29];
+const arr2 = [7, 5, 6, 8, 4]
+
+
+
+const somarArray = ()=>{
+   if(arr1[1] > arr2[1]){
+    return true
+   } else{
+    return false
+   }
+   
+}
+
+resultado = somarArray()
+
+console.log(resultado)
